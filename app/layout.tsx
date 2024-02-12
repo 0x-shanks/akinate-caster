05fa88bc868d9617838f700872897307ea3188b8
hourglasshoro <hourglasshoro.628@gmail.com>
2024-02-13 01:08:27 +0900

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,8 @@ import "./globals.css";
 
 export const metadata: Metadata = {
   // without a title, warpcast won't validate your frame
-  title: "frames.js starter",
-  description: "...",
+  title: "AkinateCaster",
+  description: "Call Genie on wapcast",
 };
 
 export default function RootLayout({
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import {
   FrameButton,
   FrameContainer,
   FrameImage,
-  FrameInput,
   FrameReducer,
   NextServerPageProps,
   getPreviousFrame,
@@ -11,23 +10,59 @@ import {
 } from "frames.js/next/server";
 import Link from "next/link";
 import { DEBUG_HUB_OPTIONS } from "./debug/constants";
-import { getTokenUrl } from "frames.js";
+import { Aki } from "aki-api";
 
 type State = {
-  active: string;
-  total_button_presses: number;
+  active: number;
+  step: number;
+  session: string;
+  signature: string;
+  uid: string;
+  frontaddr: string;
 };
 
-const initialState = { active: "1", total_button_presses: 0 };
+const initialState = {
+  active: 0,
+  step: 0,
+  session: "",
+  signature: "",
+  uid: "",
+  frontaddr: "",
+};
 
 const reducer: FrameReducer<State> = (state, action) => {
   return {
-    total_button_presses: state.total_button_presses + 1,
     active: action.postBody?.untrustedData.buttonIndex
-      ? String(action.postBody?.untrustedData.buttonIndex)
-      : "1",
+      ? action.postBody?.untrustedData.buttonIndex
+      : 0,
+    step: state.step,
+    session: state.session,
+    signature: state.signature,
+    uid: state.uid,
+    frontaddr: state.frontaddr,
   };
 };
+
+export type guess = {
+  id: string;
+  name: string;
+  id_base: string;
+  proba: string;
+  absolute_picture_path: string;
+  award_id: string;
+  corrupt: string;
+  description: string;
+  picture_path: string;
+  pseudo: string;
+  ranking: string;
+  relative: string;
+  valide_contrainte: string;
+  nsfw?: boolean;
+};
+interface winResult {
+  guessCount: number;
+  guesses: guess[];
+}
 
 // This is a react server component only
 export default async function Home({
@@ -75,45 +110,128 @@ export default async function Home({
 
   const baseUrl = process.env.NEXT_PUBLIC_HOST || "http://localhost:3000";
 
+  const region = "en";
+  const childMode = true;
+
+  let aki: Aki;
+  if (state.session == "") {
+    aki = new Aki({
+      region,
+      childMode,
+    });
+    await aki.start();
+  } else {
+    aki = new Aki({
+      region,
+      childMode,
+      currentStep: state.step,
+      session: state.session,
+      signature: state.signature,
+      uid: state.uid,
+      frontaddr: state.frontaddr,
+    });
+    await aki.startWithoutSession();
+    console.log(
+      "answer",
+      state?.active <= 2 ? state?.active - 1 : state?.active
+    );
+    await aki.step(state?.active <= 2 ? state?.active - 1 : state?.active);
+  }
+
+  const isEnd = aki.progress >= 90 || aki.currentStep >= 78;
+  let result: winResult | undefined = undefined;
+
+  if (isEnd) {
+    result = await aki.win();
+    console.log("result:", result);
+    console.log("firstGuess:", aki.answers);
+  }
+
   // then, when done, return next frame
   return (
     <div className="p-4">
-      frames.js starter kit. The Template Frame is on this page, it&apos;s in
-      the html meta tags (inspect source).{" "}
-      <Link href={`/debug?url=${baseUrl}`} className="underline">
-        Debug
-      </Link>
+      <div className="mb-8">
+        <p className="text-3xl font-bold">AkinateCaster</p>
+        <p>
+          AkinateCaster is an extension that allows akinator to be used in a
+          frame.
+        </p>
+        <p>This is only available in Warpcast</p>
+      </div>
+
+      <img alt="bg" className="mb-12 w-64 h-64 rounded-lg" src="/genie.png" />
+
+      <div className="mb-12">
+        <p className="text-lg font-bold">Author</p>
+        <a href="https://warpcast.com/nakaj1ma">
+          <div className="flex items-center space-x-4">
+            <img src="/profile.png" className="w-10 h-10 rounded-full" />
+            <p>0xshanks</p>
+          </div>
+        </a>
+      </div>
+
       <FrameContainer
         postUrl="/frames"
-        state={state}
+        state={{
+          ...state,
+          step: aki.currentStep,
+          session: aki.session ?? "",
+          signature: aki.signature ?? "",
+          uid: aki.uid ?? "",
+          frontaddr: aki.frontaddr ?? "",
+        }}
         previousFrame={previousFrame}
       >
-        {/* <FrameImage src="https://framesjs.org/og.png" /> */}
-        <FrameImage>
-          <div tw="w-full h-full bg-slate-700 text-white justify-center items-center">
-            {frameMessage?.inputText ? frameMessage.inputText : "Hello world"}
-          </div>
+        <FrameImage aspectRatio="1:1">
+          {isEnd ? (
+            <div tw="display: flex relative w-full h-full">
+              <img
+                style={{ objectFit: "cover" }}
+                alt="bg"
+                tw="absolute w-full h-full "
+                src={result?.guesses[0]?.absolute_picture_path}
+              />
+              <div tw="absolute top-0 right-0 display: flex justify-center items-center text-4xl px-20 pb-8 pt-4 bg-black/50 text-white text-wrap text-center">
+                {"Progress: " + aki.progress}
+              </div>
+              <div tw="absolute bottom-0 w-full display: flex justify-center items-center text-8xl px-20 pb-8 pt-4 bg-black/50 text-white text-wrap text-center">
+                {"I think of " + result?.guesses[0]?.name ?? ""}
+              </div>
+            </div>
+          ) : (
+            <div tw="display: flex relative w-full h-full">
+              <img
+                style={{ objectFit: "cover" }}
+                alt="bg"
+                tw="absolute w-full h-full "
+                src={`${baseUrl}/genie.png`}
+              />
+              <div tw="absolute top-0 right-0 display: flex justify-center items-center text-4xl px-20 pb-8 pt-4 bg-black/50 text-white text-wrap text-center">
+                {"Progress: " + aki.progress}
+              </div>
+              <div tw="absolute bottom-0 w-full display: flex justify-center items-center text-8xl px-20 pb-8 pt-4 bg-black/50 text-white text-wrap text-center">
+                {`Q${aki.currentStep + 1}: ${aki.question}`}
+              </div>
+            </div>
+          )}
         </FrameImage>
-        <FrameInput text="put some text here" />
-        <FrameButton>
-          {state?.active === "1" ? "Active" : "Inactive"}
-        </FrameButton>
-        <FrameButton>
-          {state?.active === "2" ? "Active" : "Inactive"}
-        </FrameButton>
-        <FrameButton
-          action="mint"
-          target={getTokenUrl({
-            address: "0x060f3edd18c47f59bd23d063bbeb9aa4a8fec6df",
-            tokenId: "123",
-            chainId: 7777777,
-          })}
-        >
-          Mint
-        </FrameButton>
-        <FrameButton action="link" target={`https://www.google.com`}>
-          External
-        </FrameButton>
+
+        {!!aki.answers[0] && !isEnd ? (
+          <FrameButton>{aki.answers[0] as string}</FrameButton>
+        ) : null}
+
+        {!!aki.answers[1] && !isEnd ? (
+          <FrameButton>{aki.answers[1] as string}</FrameButton>
+        ) : null}
+
+        {!!aki.answers[3] && !isEnd ? (
+          <FrameButton>{aki.answers[3] as string}</FrameButton>
+        ) : null}
+
+        {!!aki.answers[4] && !isEnd ? (
+          <FrameButton>{aki.answers[4] as string}</FrameButton>
+        ) : null}
       </FrameContainer>
     </div>
   );
